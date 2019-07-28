88e6f6da5cfa3df5dabd4f7c0c8000cf454c6018
davenkin <davenkin@126.com>
2019-07-28 18:10:16 +0800

    use http write method to trigger domain event publish

--- src/main/java/com/ecommerce/common/event/publish/DomainEventPublishAspect.java
+++ src/main/java/com/ecommerce/common/event/publish/DomainEventPublishAspect.java
@@ -1,14 +1,18 @@
 package com.ecommerce.common.event.publish;
 
+import com.ecommerce.common.logging.AutoNamingLoggerFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class DomainEventPublishAspect {
+    private static final Logger logger = AutoNamingLoggerFactory.getLogger();
+
     private TaskExecutor taskExecutor;
     private RabbitDomainEventPublisher publisher;
 
@@ -18,8 +22,12 @@ public class DomainEventPublishAspect {
         this.publisher = publisher;
     }
 
-    @After("@annotation(org.springframework.transaction.annotation.Transactional)")
+    @After("@annotation(org.springframework.web.bind.annotation.PostMapping) || " +
+            "@annotation(org.springframework.web.bind.annotation.PutMapping) || " +
+            "@annotation(org.springframework.web.bind.annotation.PatchMapping) || " +
+            "@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
     public void publishEvents(JoinPoint joinPoint) {
+        logger.info("Trigger domain event publish after HTTP write methods.");
         taskExecutor.execute(() -> publisher.publish());
     }
 }
--- src/main/java/com/ecommerce/common/event/publish/RabbitDomainEventBackupPublishScheduler.java
+++ src/main/java/com/ecommerce/common/event/publish/RabbitDomainEventBackupPublishScheduler.java
@@ -1,18 +1,23 @@
 package com.ecommerce.common.event.publish;
 
+import com.ecommerce.common.logging.AutoNamingLoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitDomainEventBackupPublishScheduler {
+    private static final Logger logger = AutoNamingLoggerFactory.getLogger();
+
     private RabbitDomainEventPublisher publisher;
 
     public RabbitDomainEventBackupPublishScheduler(RabbitDomainEventPublisher publisher) {
         this.publisher = publisher;
     }
 
-    @Scheduled(fixedDelay = 60000)
+    @Scheduled(fixedDelay = 30000)
     public void run() {
+        logger.info("Scheduled trigger domain event publishing.");
         publisher.publish();
     }
 
