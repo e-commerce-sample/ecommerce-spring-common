4b981d32b924ba2647b2d9710be8e33f70320d2b
davenkin <davenkin@126.com>
2019-09-04 16:10:15 +0800

    remove extra

--- src/main/java/com/ecommerce/spring/common/event/recording/mongo/MongoDomainEventConfiguration.java
+++ src/main/java/com/ecommerce/spring/common/event/recording/mongo/MongoDomainEventConfiguration.java
@@ -1,15 +1,9 @@
 package com.ecommerce.spring.common.event.recording.mongo;
 
-import com.ecommerce.shared.event.DomainEventRecorder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoDomainEventConfiguration {
 
-    @Bean
-    public DomainEventRecorder domainEventPublishingRecorder() {
-        return new MongoDomainEventRecorder();
-    }
 
 }
